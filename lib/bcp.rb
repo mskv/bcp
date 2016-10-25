89a3eaec95edbc20f56ceae5fdb835fea54a760a
Michal Podworny <michal.podworny@monterail.com>
2016-10-25 20:17:27 +0200

    Add the test runner

---
+++ lib/bcp.rb
@@ -0,0 +1,47 @@
+$:.unshift File.dirname(__FILE__)
+require "pry"
+require "ostruct"
+
+require "bcp/file_reader"
+require "bcp/solver"
+require "bcp/genome_initializer"
+require "bcp/genome_assessor"
+require "bcp/solution_assessor"
+require "bcp/crossover_picker"
+require "bcp/crossover_performer"
+require "bcp/mutator"
+
+GRAPH_FILE_PATH = File.expand_path(File.join(File.dirname(__FILE__), "../external/GEOM20.col"))
+
+random_generator = Random.new(115032730400174366788466674494640623225)
+config = OpenStruct.new(
+  random_generator: random_generator,
+  initial_genome_score: 0,
+  punishment_for_same_colour: -1,
+  prize_for_correct_colour_distance: 1,
+  punishment_for_incorrect_colour_distance: -> (colour_distance, edge_weight) {
+    - ((edge_weight - colour_distance).abs * 0.1)
+  },
+  prize_for_colour_count: -> (colour_count, node_count) {
+    (node_count - colour_count) * 0.1
+  },
+  crossover_breakpoint: -> (node_count) {
+    random_generator.rand(node_count)
+  },
+  population_size: 20,
+  mutation_chance: 0.02,
+)
+
+graph = BCP::FileReader.read(file_path: GRAPH_FILE_PATH)
+
+solver = BCP::Solver.new(
+  config: config,
+  genome_initializer: BCP::GenomeInitializer.new(config: config),
+  genome_assessor: BCP::GenomeAssessor.new(config: config),
+  solution_assessor: BCP::SolutionAssessor.new(config: config),
+  crossover_picker: BCP::CrossoverPicker.new(config: config),
+  crossover_performer: BCP::CrossoverPerformer.new(config: config),
+  mutator: BCP::Mutator.new(config: config),
+).solve(graph: graph)
+
+binding.pry
--- lib/bcp/crossover_picker.rb
+++ lib/bcp/crossover_picker.rb
@@ -7,10 +7,16 @@ module BCP
     def call(genomes:)
       sorted_genomes = genomes.sort { |x, y| x.score <=> y.score }
 
-      score_sum = genomes.reduce(0) { |acc, g| acc + g.score }
+      min_score = genomes.map(&:score).min
+      score_sum = genomes.reduce(0) { |acc, g| acc + g.score - min_score }
       genomes_with_probability_ranges = sorted_genomes.reduce([[], 0]) do |acc, genome|
         result, pivot_memo = acc
-        hi = genome.score / score_sum + pivot_memo
+        hi =
+          if score_sum == 0
+            pivot_memo
+          else
+            (genome.score - min_score) / score_sum + pivot_memo
+          end
         [
           result.concat([{ genome: genome, lo: pivot_memo, hi: hi }]),
           hi,
@@ -20,8 +26,8 @@ module BCP
       sorted_genomes.each_with_index.map do |genome, index|
         ran = @config.random_generator.rand
         pick = genomes_with_probability_ranges.select do |genome_with_range|
-          genome_with_range[:lo] <= ran && genome_with_range[:hi] > ran
-        end.first[:genome]
+          genome_with_range[:lo] <= ran #&& genome_with_range[:hi] > ran
+        end.last[:genome]
         [genome, pick]
       end.last(sorted_genomes.count / 2)
     end
